afa9448029aed3721e7d980405859b38f243b129
Isha Savaliya <155796476+IshaSavaliya25@users.noreply.github.com>
2026-08-17 10:19:24 +0530

    Time: 546 ms (90.44%), Space: 0B (100%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -1,6 +1,5 @@
--- Write your PostgreSQL query statement below
-SELECT  customer_id FROM Customer 
+# Write your MySQL query statement below
+SELECT customer_id FROM Customer c
+JOIN Product p ON c.product_key = p.product_key
 GROUP BY customer_id
-HAVING COUNT(DISTINCT product_key) = (
-    SELECT COUNT(product_key) FROM Product
-)
+HAVING COUNT( DISTINCT c.product_key) = (SELECT COUNT(*) FROM Product);
